92d9b478e3d24af078429c8ab79c43466ad32f94
sjc5182 <shiguangchen1987@gmail.com>
2018-12-19 23:13:16 -0800

    modules delete

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import './styles/styles.scss';
 
 
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Cable bill', amount: 2500 }));
+store.dispatch(addExpense({ description: 'Cable abc bill', amount: 2500 }));
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill', createAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
